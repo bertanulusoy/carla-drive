6372c7d835aeb8323808623499964d4dda9e7182
Ömer Tuğrul Gürbüz <otugrul2012@gmail.com>
2026-01-01 15:22:33 +0300

    Save changes

--- src/carla_drive/common/logging.py
+++ src/carla_drive/common/logging.py
@@ -5,3 +5,4 @@ from carla_drive.common.logging import get_logger
 log = get_logger(carla_drive)
 log.info("app_started", mode="dev")
 
+poetry run python -c "from carla_drive.common.logging import get_logger; get_logger('x').info('hello')"
