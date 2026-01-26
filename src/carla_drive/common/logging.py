604937a884317567b6a885cceb229dc8b4697446
Ömer Tuğrul Gürbüz <otugrul2012@gmail.com>
2026-01-26 21:41:30 +0300

    Save changes

--- src/carla_drive/common/logging.py
+++ src/carla_drive/common/logging.py
@@ -1,12 +1,21 @@
-get_logger(name: str)
-
 import logging
 
-logging.basicConfig(level=logging.INFO)
+def get_logger(name: str):
+    """Create and return a logger with the given name."""
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
 
-logging.35(35)
-logging.Ahmet(Ahmet)
-logging.Oscar(Oscar)
-logging.biktim(biktim)
 
-print(level: logging.INFO.getLevelNumber(2))
+if __name__ == "__main__":
+    logger = get_logger("test")
+    logger.info("Logging modülü çalışıyor!")
+    logger.debug("Bu bir debug mesajı")
+    logger.warning("Bu bir uyarı mesajı")
