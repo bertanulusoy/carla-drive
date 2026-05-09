96e6408c2d638e868d91713bedc0d9771bf5f254
Ömer Tuğrul Gürbüz <otugrul2012@gmail.com>
2026-05-09 18:19:12 +0300

    feat(sensors): define rgb camera helper interface

--- tests/unit/test_sensors_import.py
+++ tests/unit/test_sensors_import.py
@@ -1,4 +1,5 @@
 """Test basic import of sensors module."""
+import inspect
 
 
 def test_import_spawn_rgb_camera():
@@ -6,3 +7,13 @@ def test_import_spawn_rgb_camera():
     from carla_drive.feature.sensors import spawn_rgb_camera
 
     assert callable(spawn_rgb_camera)
+
+
+def test_spawn_rgb_camera_signature():
+    """Test that spawn_rgb_camera has the expected signature."""
+    from carla_drive.feature.sensors import spawn_rgb_camera
+
+    sig = inspect.signature(spawn_rgb_camera)
+    params = list(sig.parameters.keys())
+
+    assert params == ['vehicle', 'width', 'height', 'callback']
